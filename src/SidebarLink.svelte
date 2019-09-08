3c4ac31dce4459a6219b61a2e017cc4b8294f337
Philipp Schneider <philipp@schneiders.space>
2019-09-08 19:08:34 +0200

    Adds group indicator + user triggered collapsing.
    When a sidebar link has child routes: Renders a button indicating the
    link group's collapse status and allowing the user to toggle it when
    clicking.
    
    Signed-off-by: Philipp Schneider <philipp@schneiders.space>

--- src/SidebarLink.svelte
+++ src/SidebarLink.svelte
@@ -5,17 +5,26 @@
 	export let childRoutes = null;
 	export let level = 1;
 
-	$: marginLeft = (level - 1) * 20;
+	let groupClosed = false;
+
 	$: active = url === route;
+	$: marginLeft = (level - 1) * 40;
+	$: groupOpen = childRoutes && !groupClosed;
+
+	const toggleGroup = () => groupClosed = !groupClosed;
 </script>
 
 <li class:active={active} style='margin-left: {marginLeft}px'>
+	{#if childRoutes}
+		<button class='group-indicator' class:open={groupOpen} on:click={toggleGroup}>> </button>
+	{/if}
+
 	<a href={route} class:active={active}>
 		{name}
 	</a>
 </li>
 
-{#if childRoutes}
+{#if groupOpen}
 	{#each childRoutes as route (route.route)}
 		<svelte:self url={url} {...route} level={level + 1}/>
 	{/each}
@@ -24,5 +33,13 @@
 <style>
 	.active {
 		font-weight: bold;
+	}
+
+	.group-indicator {
+		display: inline-block;
+	 }
+
+	.group-indicator.open {
+		transform: rotate(90deg);
 	}
 </style>
