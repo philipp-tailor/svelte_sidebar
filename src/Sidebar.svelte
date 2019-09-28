d1f3d37c98862088a891bdb561d6cbba35fdd346
Philipp Schneider <philipp@schneiders.space>
2019-09-28 20:55:31 +0200

    Adds theming to the `Sidebar`.
    The theme is starting with a few basic properties. It can be
    overridden by setting the `theme` prop on `Sidebar`.
    The theme is variables are passed from JS to CSS variables, where they
    can be used in `Sidebar` and all child components.

--- src/Sidebar.svelte
+++ src/Sidebar.svelte
@@ -2,26 +2,47 @@
 	import { setContext } from 'svelte';
 	import SidebarGroup from './SidebarGroup.svelte';
 
+	const defaultTheme = {
+		color_link: '#F7F7F2',
+		color_linkActive: '#F4442E',
+		color_linkHover: '#FCA311',
+		backgroundColor_nav: '#555B6E',
+		fontSize: '1.2rem',
+	};
+
 	export let activeUrl;
 	export let routes = [];
 	export let onLinkClick = null;
+	export let theme = {};
 
 	setContext('sidebarContext', { activeUrl, onLinkClick });
+
+	$: combinedTheme = { ...defaultTheme, ...theme };
+	$: styleString = [
+			`--color_link: ${combinedTheme.color_link}`,
+			`--color_linkActive: ${combinedTheme.color_linkActive}`,
+			`--color_linkHover: ${combinedTheme.color_linkHover}`,
+			`--backgroundColor_nav: ${combinedTheme.backgroundColor_nav}`,
+			`--fontSize: ${combinedTheme.fontSize}`,
+		].join('; ');
 </script>
 
-<nav>
+<nav style={styleString}>
 	<SidebarGroup routes={routes} isRoot={true}/>
 </nav>
 
 <style>
 	nav {
-		background-color: #3d3e40;
 		height: 100vh;
 		min-width: 320px;
 		width: max(320px, 25vw);
 		box-sizing: border-box;
 		overflow-y: auto;
 		z-index: 2;
+		padding: calc(var(--fontSize) + 10px);
+		background-color: var(--backgroundColor_nav);
+		box-shadow: calc(var(--fontSize) / 10) 0 var(--fontSize) 0 var(--backgroundColor_nav);
+		font-size: var(--fontSize);
 		user-select: none;
 		-webkit-user-select: none;
 		-moz-user-select: none;
