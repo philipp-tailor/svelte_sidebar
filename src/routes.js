ebf2632302c1b705a18ee3b06fd86e8a3fa3e29d
Philipp Schneider <philipp@schneiders.space>
2019-09-29 12:11:57 +0200

    Adds more challenging examples to `routes.js`.
    * Makes name of "/settings/org" really long to enforce a line break.
      Right now the indent of a wrapped entry is not different from a
      separate route.
    * Adds a disabled property to the "/checkout/confirmation/error"
      route. The disabled case is going to be addressed in the following.
    * The file has been autoformatted.

--- src/routes.js
+++ src/routes.js
@@ -1,69 +1,70 @@
 export const unAuthenticatedRoutes = [
-  {
-    name: "Login",
-    route: "/login"
-  },
-  {
-    name: "Signup",
-    route: "/signup"
-  },
-  {
-    name: "Password Reset",
-    route: "/password-reset"
-  },
-  {
-    name: "Terms Of Service",
-    route: "/terms-of-service"
-  }
+	{
+		name: "Login",
+		route: "/login"
+	},
+	{
+		name: "Signup",
+		route: "/signup"
+	},
+	{
+		name: "Password Reset",
+		route: "/password-reset"
+	},
+	{
+		name: "Terms Of Service",
+		route: "/terms-of-service"
+	}
 ];
 
 export const authenticatedRoutes = [
-  {
-    name: "Home",
-    route: "/"
-  },
-  {
-    name: "Settings",
-    route: "/settings",
-    childRoutes: [
-      {
-        name: "User Settings",
-        route: "/settings/user"
-      },
-      {
-        name: "Organisation Settings",
-        route: "/settings/org"
-      }
-    ]
-  },
-  {
-    name: "Checkout",
-    route: "/checkout",
-    childRoutes: [
-      {
-        name: "Cart",
-        route: "/checkout/cart"
-      },
-      {
-        name: "Payment Details",
-        route: "/checkout/payment"
-      },
-      {
-        name: "Confirm",
-        route: "/checkout/confirmation",
-        childRoutes: [
-          {
-            name: "Order Success",
-            route: "/checkout/confirmation/success"
-          },
-          {
-            name: "Order Failure",
-            route: "/checkout/confirmation/error"
-          }
-        ]
-      }
-    ]
-  }
+	{
+		name: "Home",
+		route: "/"
+	},
+	{
+		name: "Settings",
+		route: "/settings",
+		childRoutes: [
+			{
+				name: "User Settings",
+				route: "/settings/user"
+			},
+			{
+				name: "Organisation Settings (Team Management & Billing)",
+				route: "/settings/org"
+			}
+		]
+	},
+	{
+		name: "Checkout",
+		route: "/checkout",
+		childRoutes: [
+			{
+				name: "Cart",
+				route: "/checkout/cart"
+			},
+			{
+				name: "Payment Details",
+				route: "/checkout/payment"
+			},
+			{
+				name: "Confirm",
+				route: "/checkout/confirmation",
+				childRoutes: [
+					{
+						name: "Order Success",
+						route: "/checkout/confirmation/success"
+					},
+					{
+						name: "Order Failure",
+						route: "/checkout/confirmation/error",
+						disabled: true
+					}
+				]
+			}
+		]
+	}
 ];
 
 const routes = [...unAuthenticatedRoutes, ...authenticatedRoutes];
