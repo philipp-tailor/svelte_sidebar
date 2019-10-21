7d3866abea1919da2c4fc5eb5e6430f6c6d03031
Philipp Schneider <philipp@schneiders.space>
2019-10-21 09:09:08 +0200

    Adds `theme` configuration to playground.
    All current theme properties can now be set live from the playground.

--- src/App.svelte
+++ src/App.svelte
@@ -17,6 +17,17 @@
 		activeUrl: '/checkout/confirmation/success',
 		routes: authenticatedRoutes,
 		open: window.innerWidth > 720,
+		theme: {
+			backgroundColor_linkActive: '#F4442E',
+			backgroundColor_nav: '#555B6E',
+			color_link: '#F7F7F2',
+			color_linkHover: '#FCA311',
+			fontSize: '1.2rem',
+			maxWidth_nav: '20vw',
+			minWidth_nav: '320px',
+			opacity_linkDisabled: '0.5',
+			opacity_linkInactive: '0.75'
+		}
 	})
 
 	const getDeepObjectCopy = value => JSON.parse(JSON.stringify(value))
@@ -60,6 +71,43 @@
 	<textarea
 		on:input={e => (sidebarConfig.routes = JSON.parse(e.target.value))}
 		value={JSON.stringify(sidebarConfig.routes)} />
+
+	<input type="color" bind:value={sidebarConfig.theme.backgroundColor_linkActive} />
+
+	<input type="color" bind:value={sidebarConfig.theme.backgroundColor_nav} />
+
+	<input type="color" bind:value={sidebarConfig.theme.color_link} />
+
+	<input type="color" bind:value={sidebarConfig.theme.color_linkHover} />
+
+	<input
+		type="range"
+		on:input={e => (sidebarConfig.theme.fontSize = `${e.target.value}rem`)}
+		value={Number.parseFloat(sidebarConfig.theme.fontSize)}
+		min="0.1"
+		max="3"
+		step="0.1" />
+
+	<input
+		type="range"
+		on:input={e => (sidebarConfig.theme.maxWidth_nav = `${e.target.value}vw`)}
+		value={Number.parseFloat(sidebarConfig.theme.maxWidth_nav)}
+		min="5"
+		max="100" />
+
+	<input
+		type="range"
+		on:input={e => {
+			sidebarConfig.theme.minWidth_nav = `${e.target.value}px`
+			sidebarConfig.open = window.innerWidth > 720 && window.innerWidth / 2 > e.target.value
+		}}
+		value={Number.parseFloat(sidebarConfig.theme.minWidth_nav)}
+		min="5"
+		max="1920" />
+
+	<input type="range" bind:value={sidebarConfig.theme.opacity_linkDisabled} min="0.1" max="1" step="0.1" />
+
+	<input type="range" bind:value={sidebarConfig.theme.opacity_linkInactive} min="0.1" max="1" step="0.1" />
 
 	<button on:click={resetSidebarConfig}>reset</button>
 </main>
