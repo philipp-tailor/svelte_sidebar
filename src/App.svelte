42c209a9570e28aca2e62fe2f089ddff1aa330fe
Philipp Schneider <philipp@schneiders.space>
2019-11-15 18:34:18 +0100

    Updates `App` component's template markup.
    The changes will allow `App` to look as desired.

--- src/App.svelte
+++ src/App.svelte
@@ -75,115 +75,160 @@
 </style>
 
 <svelte:component this={Sidebar} {...sidebarConfig} {onLinkClick} />
+
 <main class="route-content">
-	<label for="activeUrl">Active URL</label>
-	<select bind:value={sidebarConfig.activeUrl} id="activeUrl">
-		{#each selectableRoutes as { name, route }}
-			<option value={route}>{name}</option>
+	<h2 class="heading">
+		Play<wbr />ground
+	</h2>
+	<div class="playground">
+		<div class="form-explainer">
+			<div>
+				Customise the sidebar on the left with the form below. The props passed to the component can be seen on
+				the right. Changes are displayed live.
+			</div>
+			<button
+				on:click={resetSidebarConfig}
+				reset form
+			</button>
+		</div>
+
+		<fieldset>
+			<legend>Color</legend>
+			<div class="fieldset-container">
+				<label for="backgroundColor_nav" class="color-input">
+					Back<wbr />ground Color of Sidebar:
+					<input type="color" bind:value={sidebarConfig.theme.backgroundColor_nav} id="backgroundColor_nav" />
+				</label>
+
+				<label for="backgroundColor_linkActive" class="color-input">
+					Back<wbr />ground Color of Active Links:
+					<input
+						type="color"
+						bind:value={sidebarConfig.theme.backgroundColor_linkActive}
+						id="backgroundColor_linkActive" />
+				</label>
+
+				<label for="color_link" class="color-input">
+					Color of Link Text:
+					<input type="color" bind:value={sidebarConfig.theme.color_link} id="color_link" />
+				</label>
+
+				<label for="color_linkHover" class="color-input">
+					Hover Color of Link Text:
+					<input type="color" bind:value={sidebarConfig.theme.color_linkHover} id="color_linkHover" />
+				</label>
+			</div>
+		</fieldset>
+
+		<fieldset>
+			<legend>Opacity</legend>
+
+			<div class="fieldset-container">
+				<label for="opacity_linkDisabled">
+					Opacity of Disabled Links:
+					<input
+						type="number"
+						bind:value={sidebarConfig.theme.opacity_linkDisabled}
+						min="0.1"
+						max="1"
+						step="0.1"
+						id="opacity_linkDisabled" />
+				</label>
+
+				<label for="opacity_linkInactive">
+					Opacity of Links Which Are Not Part of the Active Route:
+					<input
+						type="number"
+						bind:value={sidebarConfig.theme.opacity_linkInactive}
+						min="0.1"
+						max="1"
+						step="0.1"
+						id="opacity_linkInactive" />
+				</label>
+			</div>
+		</fieldset>
+
+		<fieldset>
+			<legend>Sizing</legend>
+			<div class="fieldset-container">
+				<label for="fontSize">
+					Font Size in rem:
+					<input
+						type="number"
+						on:input={e => (sidebarConfig.theme.fontSize = `${e.target.value}rem`)}
+						value={Number.parseFloat(sidebarConfig.theme.fontSize)}
+						min="0.1"
+						max="3"
+						step="0.1"
+						id="fontSize" />
+				</label>
+
+				<label for="maxWidth_nav">
+					Sidebar Maximum Width in px:
+					<input
+						type="number"
+						on:input={e => (sidebarConfig.theme.maxWidth_nav = `${(e.target.value / window.innerWidth) * 100}vw`)}
+						value={(Number.parseFloat(sidebarConfig.theme.maxWidth_nav) / 100) * window.innerWidth}
+						min="10"
+						step="50"
+						max={window.innerWidth}
+						id="maxWidth_nav" />
+				</label>
+
+				<label for="minWidth_nav">
+					Sidebar Minimum Width in px:
+					<input
+						type="number"
+						on:input={e => {
+							sidebarConfig.theme.minWidth_nav = `${e.target.value}px`
+							sidebarConfig.open = window.innerWidth > 720 && window.innerWidth / 2 > e.target.value
+						}}
+						value={Number.parseFloat(sidebarConfig.theme.minWidth_nav)}
+						min="0"
+						step="50"
+						max={window.innerWidth}
+						id="minWidth_nav" />
+				</label>
+			</div>
+		</fieldset>
+
+		<fieldset class="content-fieldset">
+			<legend>Content</legend>
+
+			<label for="activeUrl">Active URL</label>
+			<select bind:value={sidebarConfig.activeUrl} id="activeUrl">
+				{#each selectableRoutes as { name, route }}
+					<option value={route}>{name}</option>
+				{/each}
+			</select>
+
+			<label for="routes">Navigation Content</label>
+			<textarea
+				on:input={e => (sidebarConfig.routes = JSON.parse(e.target.value))}
+				value={editableRoutes}
+				rows={editableRoutes.split('\n').length + 3}
+				required
+				id="routes" />
+		</fieldset>
+	</div>
+
+	<aside>
+		<legend>Result</legend>
+		<span class="control-character">&lt;</span>
+		<span class="html-tag">Sidebar</span>
+		{#each Object.entries(sidebarConfig) as prop (prop[0])}
+			<p class="component-prop">
+				<span>{prop[0]}</span>
+				<span class="control-character">="</span>
+				<span class="component-prop-value">
+					{#if JSON.stringify(prop[1]).split('\n').length > 1}
+						<br />
+					{/if}
+					{JSON.stringify(prop[1], null, 4).replace(/^"|"$/gi, '')}
+				</span>
+				<span class="control-character">"</span>
+			</p>
 		{/each}
-	</select>
-
-	<label for="routes">Navigation Content</label>
-	<textarea
-		on:input={e => (sidebarConfig.routes = JSON.parse(e.target.value))}
-		value={JSON.stringify(sidebarConfig.routes)}
-		id="routes" />
-
-	<fieldset>
-		<legend>Colors</legend>
-
-		<label for="backgroundColor_nav">Background Color of Sidebar:</label>
-		<input
-			type="color"
-			bind:value={sidebarConfig.theme.backgroundColor_nav}
-			id="backgroundColor_nav"
-			tabindex="0" />
-		{sidebarConfig.theme.backgroundColor_nav}
-		<br />
-
-		<label for="backgroundColor_linkActive">Background Color of Active Links:</label>
-		<input
-			type="color"
-			bind:value={sidebarConfig.theme.backgroundColor_linkActive}
-			id="backgroundColor_linkActive" />
-		{sidebarConfig.theme.backgroundColor_linkActive}
-		<br />
-
-		<label for="color_link">Color of Link Text:</label>
-		<input type="color" bind:value={sidebarConfig.theme.color_link} id="color_link" />
-		{sidebarConfig.theme.color_link}
-		<br />
-
-		<label for="color_linkHover">Hover Color of Link Text:</label>
-		<input type="color" bind:value={sidebarConfig.theme.color_linkHover} id="color_linkHover" />
-		{sidebarConfig.theme.color_linkHover}
-	</fieldset>
-
-	<fieldset>
-		<legend>Opacity</legend>
-
-		<label for="opacity_linkDisabled">Opacity of Disabled Links:</label>
-		<input
-			type="range"
-			bind:value={sidebarConfig.theme.opacity_linkDisabled}
-			min="0.1"
-			max="1"
-			step="0.1"
-			id="opacity_linkDisabled" />
-		{sidebarConfig.theme.opacity_linkDisabled}
-		<br />
-
-		<label for="opacity_linkInactive">Opacity of Links Which Are Not Part of the Active Route:</label>
-		<input
-			type="range"
-			bind:value={sidebarConfig.theme.opacity_linkInactive}
-			min="0.1"
-			max="1"
-			step="0.1"
-			id="opacity_linkInactive" />
-		{sidebarConfig.theme.opacity_linkInactive}
-	</fieldset>
-
-	<fieldset>
-		<legend>Sizes</legend>
-
-		<label for="fontSize">Font Size:</label>
-		<input
-			type="range"
-			on:input={e => (sidebarConfig.theme.fontSize = `${e.target.value}rem`)}
-			value={Number.parseFloat(sidebarConfig.theme.fontSize)}
-			min="0.1"
-			max="3"
-			step="0.1"
-			id="fontSize" />
-		{sidebarConfig.theme.fontSize}
-		<br />
-
-		<label for="maxWidth_nav">Sidebar Max Width:</label>
-		<input
-			type="range"
-			on:input={e => (sidebarConfig.theme.maxWidth_nav = `${e.target.value}vw`)}
-			value={Number.parseFloat(sidebarConfig.theme.maxWidth_nav)}
-			min="5"
-			max="100"
-			id="maxWidth_nav" />
-		{sidebarConfig.theme.maxWidth_nav}
-		<br />
-
-		<label for="minWidth_nav">Sidebar Min Width:</label>
-		<input
-			type="range"
-			on:input={e => {
-				sidebarConfig.theme.minWidth_nav = `${e.target.value}px`
-				sidebarConfig.open = window.innerWidth > 720 && window.innerWidth / 2 > e.target.value
-			}}
-			value={Number.parseFloat(sidebarConfig.theme.minWidth_nav)}
-			min="5"
-			max="1920"
-			id="minWidth_nav" />
-		{sidebarConfig.theme.minWidth_nav}
-		<br />
-	</fieldset>
-
-	<button on:click={resetSidebarConfig}>reset</button>
+		<span class="control-character">/&gt;</span>
+	</aside>
 </main>
