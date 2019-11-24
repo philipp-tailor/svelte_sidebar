b6961bacd7a775c33036bd51280cc1b4b43dfa3d
Philipp Schneider <philipp@schneiders.space>
2019-11-24 12:04:55 +0100

    Moves global style to `App`...
    ... to decrease the number of blocking requests in the render paths.
    
    While HTTP2 sped up loading of more small files, it's still less
    optimal for blocking styles to have multiple blocking requests in the
    render path then not, as the total stylesheet is still smaller than
    the ~14kb that fit into one roundtrip's request payload.
    
    This change has the nice side-effect that now all stiles are minified.
    Previously `global.css` was not part of the build pipeline and
    therefore not minimised.

--- src/App.svelte
+++ src/App.svelte
@@ -23,6 +23,38 @@
 </script>
 
 <style>
+	:global(:root) {
+		supported-color-scheme: light dark;
+		color-scheme: light dark;
+
+		/* the minimum ever available width for content (iPhone SE) */
+		--min-available-width: 240px;
+
+		--dark-primary: #1e1e1e;
+		--dark-secondary: #555b6e;
+		--light-primary: #eaeef1;
+		--light-secondary: #f7f7f2;
+		--bg-white: white;
+		--bg-blue-primary: #003649;
+		--accent-red: #f4442e;
+		--accent-orange: #fca311;
+
+		color: var(--dark-primary);
+	}
+
+	:global(body) {
+		display: flex;
+		height: 100vh;
+		margin: 0;
+		font-family: sans-serif;
+	}
+
+	:global(#svelte-sidebar) {
+		flex-grow: 5;
+		flex-shrink: 1;
+		flex-basis: 320px;
+	}
+
 	.route-content {
 		background-color: var(--light-primary);
 		overflow-y: auto;
@@ -35,8 +67,24 @@
 	}
 
 	@media (prefers-color-scheme: dark) {
+		:global(:root) {
+			color: var(--light-secondary);
+		}
+
 		.route-content {
 			background-color: var(--dark-secondary);
+		}
+	}
+
+	@media only screen and (max-width: 380px) {
+		:global(html) {
+			font-size: 13px;
+		}
+	}
+
+	@media only screen and (min-width: 380px) {
+		:global(html) {
+			font-size: 16px;
 		}
 	}
 </style>
